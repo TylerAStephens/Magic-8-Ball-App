7b0bd2d9baf816e426fb63728e0e6b45fc504450
TylerAStephens <tylerstephens16@yahoo.com>
2018-10-29 04:15:25 -0400

    added logic for app launch

--- Magic 8 Ball/Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    //Array for ball images
+    var ballRandomNumber: Int = 0
+    //variable to hold random number
+    
+    @IBOutlet weak var imageView: UIImageView!
+    //Outlet for main image
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        ballRandomNumber = Int.random(in: 0 ... 4)
+        //Random Number Generator
+        imageView.image = UIImage(named: ballArray[ballRandomNumber])
+        //Random image generator upon launch
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func askButtonPressed(_ sender: Any) {
+    }
+    //Button action call
 }
 
